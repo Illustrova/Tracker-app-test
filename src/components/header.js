c13ec445cc3cc692bef3e26262a8aac02d137df8
Irina Illustrova <illustrova@gmail.com>
2021-07-03 17:10:38 +0300

    add button go back

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,15 @@
 export default function Header() {
   return (
-    <header className="p-8 flex items-center ">
-      <span role="img" aria-label="Clock" className="text-4xl">
-        🕑
-      </span>
-      <p className="ml-4  text-2xl  font-bold font-mono">Time Tracker App</p>
+    <header className="flex items-center justify-between p-8">
+      <div className="flex">
+        <span role="img" aria-label="Clock" className="text-4xl">
+          🕑
+        </span>
+        <p className="ml-4 font-mono text-2xl font-bold">Time Tracker App</p>
+      </div>
+      <a className="text-xl underline hover:no-underline" href="../">
+        Back to other implementations
+      </a>
     </header>
   );
 }
